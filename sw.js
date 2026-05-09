b5de30b8914bf0bc76b59af919f4310b4313ca13
JudylovesAI <1790232251@qq.com>
2026-05-09 17:55:42 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'picture-book-v1';
+const CACHE_NAME = 'picture-book-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -7,24 +7,39 @@ const ASSETS = [
   './icon-512.png'
 ];
 
+// Install: pre-cache assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  // Force activate immediately
+  self.skipWaiting();
 });
 
+// Fetch: Network First strategy (always try network first for updates)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    fetch(event.request).then(response => {
+      // If network succeeds, cache the new version and return it
+      if (response.ok) {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+      }
+      return response;
+    }).catch(() => {
+      // Network failed, try cache
+      return caches.match(event.request);
     })
   );
 });
 
+// Activate: clean up old caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then(keys => 
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
+  // Take control of all clients immediately
+  self.clients.claim();
 });
